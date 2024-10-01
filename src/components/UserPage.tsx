7d661e033508d900fa2e3a9103bb5b19bac8f250
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-10-01 11:37:46 +0200

    feat: read and write to pods

--- src/components/UserPage.tsx
+++ src/components/UserPage.tsx
@@ -1,12 +1,27 @@
 import { useContext, useEffect, useState } from "react";
-import { getPodUrlAll } from "@inrupt/solid-client";
+import {
+  addStringNoLocale,
+  addUrl,
+  createSolidDataset,
+  createThing,
+  getPodUrlAll,
+  getSolidDataset,
+  getStringNoLocale,
+  getThingAll,
+  saveSolidDatasetAt,
+  setThing,
+} from "@inrupt/solid-client";
 import { fetch } from "@inrupt/solid-client-authn-browser";
 import { AuthContext } from "../context/AuthContext";
+import { SCHEMA_INRUPT, RDF, AS } from "@inrupt/vocab-common-rdf";
 
 export default function UserPage() {
   const { user, signOut } = useContext(AuthContext);
   const [pods, setPods] = useState<string[]>([]);
   const [selectedPod, setSelectedPod] = useState<string | null>(null);
+  const [readingList, setReadingList] = useState<(string | null)[]>([]);
+  const [input, setInput] = useState("");
+  const [error, setError] = useState("");
 
   useEffect(() => {
     if (user) {
@@ -15,12 +30,77 @@ export default function UserPage() {
   }, []);
 
   useEffect(() => {
-    console.log("Selected pod:", selectedPod);
+    if (selectedPod) {
+      fetchReadingList();
+    }
   }, [selectedPod]);
+
+  const readingListUrl = (pod: string) => `${pod}/reading-list`;
 
   async function getPods() {
     const pods = await getPodUrlAll(user!.webID, { fetch: fetch });
     setPods(pods);
+  }
+
+  async function fetchReadingList() {
+    if (!selectedPod) return;
+
+    try {
+      const readingList = await getSolidDataset(readingListUrl(selectedPod), {
+        fetch: fetch,
+      });
+
+      const items = getThingAll(readingList).map((item) =>
+        getStringNoLocale(item, SCHEMA_INRUPT.name)
+      );
+      setReadingList(items);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function addToReadingList() {
+    setError("");
+
+    if (!selectedPod) {
+      setError("Please select a pod before writing...");
+      return;
+    }
+
+    if (input.trim() === "") {
+      setError("Field cannot be empty...");
+      return;
+    }
+
+    let myReadingList;
+    try {
+      myReadingList = await getSolidDataset(readingListUrl(selectedPod), {
+        fetch: fetch,
+      });
+    } catch (err) {
+      myReadingList = createSolidDataset();
+    }
+
+    let item = createThing({ name: input });
+    item = addUrl(item, RDF.type, AS.Article);
+    item = addStringNoLocale(item, SCHEMA_INRUPT.name, input);
+    myReadingList = setThing(myReadingList, item);
+
+    try {
+      const savedReadingList = await saveSolidDatasetAt(
+        readingListUrl(selectedPod),
+        myReadingList,
+        { fetch: fetch }
+      );
+      const updatedReadingList = getThingAll(savedReadingList);
+      const mappedReadingList = updatedReadingList.map((item) =>
+        getStringNoLocale(item, SCHEMA_INRUPT.name)
+      );
+
+      setReadingList(mappedReadingList);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
@@ -49,17 +129,22 @@ export default function UserPage() {
       <div>
         <h2>Reading list</h2>
         <ul>
-          {pods.map((pod) => {
-            return <li key={pod}>{pod}</li>;
+          {readingList.map((item) => {
+            return item && <li key={item}>{item}</li>;
           })}
         </ul>
       </div>
 
       <div>
         <h2>Add item to reading list</h2>
-        <form>
-          <textarea placeholder="What to read..." />
-          <button>Add</button>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="What to read..."
+          />
+          <button onClick={addToReadingList}>Add</button>
+          {error && <p>{error}</p>}
         </form>
       </div>
     </>
